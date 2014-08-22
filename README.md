3b2a837df7c212024e544eab4a8234ad30e6968b
Gilles Polart-Donat <gpolart@lutraconseil.fr>
2014-08-22 10:58:51 +0200

    Add Wisconsin breat cancer test

--- README.md
+++ README.md
@@ -11,8 +11,8 @@ It's part of a work realized for Institut Mines-Telecom in Teralab project : htt
 I use this dataset for KMeans clustering with verification from the diagnostic. It's a small dataset (569 lines) usefull for testing purpose. The CSV file is used directly and vectorized by Scala program.A So we need only to put CSV file on HDFS and call the spar-summit programm with arguments :
 `spark-submit --class fr.teralabdatascience.tests.BreastCancer --deploy-mode client --master yarn target/breastcancer-0.0.1-SNAPSHOT.jar wisconsin_data_breast_cancer_data_org.csv`
 
-Diagnostic dataset : https://archive.ics.uci.edu/ml/datasets/Breast+Cancer+Wisconsin+%28Diagnostic%29
-Original dataset : https://archive.ics.uci.edu/ml/datasets/Breast+Cancer+Wisconsin+%28Original%29
+- Diagnostic dataset : https://archive.ics.uci.edu/ml/datasets/Breast+Cancer+Wisconsin+%28Diagnostic%29
+- Original dataset : https://archive.ics.uci.edu/ml/datasets/Breast+Cancer+Wisconsin+%28Original%29
 
 ## Higg's boson dataset ##
 
---
+++ src/main/scala/fr/teralab-datascience/tests/BreastCancer.scala
@@ -0,0 +1,65 @@
+// =======================================================================
+// Institut Mines Telecom - Teralab
+// =======================================================================
+package fr.teralab-datascience.tests
+
+
+import org.apache.spark.SparkContext
+import org.apache.spark.SparkContext._
+import org.apache.spark.SparkConf
+import org.apache.spark.mllib.clustering.KMeans
+import org.apache.spark.mllib.linalg.Vectors
+
+object BreastCancer {
+  var path: String = _
+  var num_iter: Int = 20
+  var numIterations: Int = 2
+
+  // Print argument list
+  def usage() {
+    println("Arguments :")
+    println("    path     : HDFS path for CSV file from Winsconsin Breast Cancer test case")
+    println("    num_iter : K-Means iteration number (default to 20")
+  }
+
+  // Print argument list
+  def test_arguments(args: Array[String]) {
+    if ( (args.length < 1) || ( args(0).length <= 0) ) {
+      usage()
+      System.exit(1)
+    }
+    path = args(0)
+    if (args.length > 1) {
+      numIterations = args(1).toInt
+    }
+  }
+
+  // Function to skip beginning of Array
+  def tail(a: Array[String], h: Int) = { a.slice(h, a.length) }
+
+  // Main method
+  def main(args: Array[String]) {
+    // Arguments testing
+    test_arguments(args);
+
+    // Spark initialisation
+    val sparkConf = new SparkConf().setAppName("BreastCancer")
+    val sc = new SparkContext(sparkConf)
+
+    // Convert CSV file to Vectors usable for MLlib
+    val data = sc.textFile(path).map {
+      line => Vectors.dense(tail(line.split(','),2).map(_.toDouble))
+    }.cache()
+
+    // Calculate
+    val numClusters = 2
+    val clusters = KMeans.train(data, numClusters, numIterations)
+
+    // Evaluate clustering by computing Within Set Sum of Squared Errors
+    val WSSSE = clusters.computeCost(data)
+    println("Within Set Sum of Squared Errors = " + WSSSE)
+  }
+}
+
+// vim: sw=2:ts=2:ai
+
